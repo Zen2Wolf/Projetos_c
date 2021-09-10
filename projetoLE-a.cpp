8f33259684cfa3596b137c2d158e64d3939ec99a
Zen2Wolf <85023505+Zen2Wolf@users.noreply.github.com>
2021-09-10 07:21:17 -0300

    Update and rename projetoLE/1HID-LE-Enzo Dante-Isaac de Oliveira-Rafael Paroni.cpp to projetoLE-a.cpp

--- projetoLE-a.cpp
+++ projetoLE-a.cpp
@@ -1,5 +1,3 @@
-// 1HID -- Enzo Dante, Issac de Oliveira, Rafael Paroni
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
@@ -13,21 +11,21 @@ main(){
 		contam[12] = 0;
 		contam[13] = 0;
 		valor = 10;
-		printf("Digite os n˙meros de seu CPF, um n˙mero de cada vez!\n");
+		printf("Digite os n√∫meros de seu CPF, um n√∫mero de cada vez!\n");
 		for(x = 0; x <= 10; x++){
 			scanf("%d", &cpf[x]);
 			while((cpf[x] < 0) || (cpf[x] > 9)){
-				printf("Digite o n˙mero de seu cpf corretamente!!:\n");
+				printf("Digite o n√∫mero de seu cpf corretamente!!:\n");
 				scanf("%d", &cpf[x]);
 			}
 		}
 		for(x = 0; x <= 8; x++){
 				
-			Ccpf[0][x] = cpf[x]; //cpf digitado, apenas os 9 n˙meros do cpf					
-			Ccpf[1][x] = valor; //valor abaixo dos 9 n˙meros do cpf (de 10 atÈ 2)
+			Ccpf[0][x] = cpf[x]; //cpf digitado, apenas os 9 n√∫meros do cpf					
+			Ccpf[1][x] = valor; //valor abaixo dos 9 n√∫meros do cpf (de 10 at√© 2)
 			valor = valor - 1;
 			contam[x] = Ccpf[0][x] * Ccpf[1][x]; //valores multiplicados exemplo:(linha-1-coluna-1 * linha-2-coluna-1)
-			contam[12] = contam[12] + contam[x]; //dentro de contam[10] ter· a soma dos valores multiplicados de dentro dele (do 1∫ teste)	
+			contam[12] = contam[12] + contam[x]; //dentro de contam[10] ter√° a soma dos valores multiplicados de dentro dele (do 1¬∫ teste)	
 		}		
 		resto = contam[12]%11;
 		if(resto < 2){
@@ -55,20 +53,20 @@ main(){
 			digit10_11[1] = 11 - resto;
 		}
 		if((digit10_11[0] == cpf[9]) && (digit10_11[1] == cpf[10])){
-			printf("O CPF digitado È v·lido!\n");
+			printf("O CPF digitado √© v√°lido!\n");
 			porcpfs[0]++;
-				 //total de cpfs v·lidos
+				 //total de cpfs v√°lidos
 		}
 		else{
-			printf("O CPF digitado È inv·lido!!!\n");
+			printf("O CPF digitado √© inv√°lido!!!\n");
 			porcpfs[1]++;
-				 //total de cpfs inv·lidos
+				 //total de cpfs inv√°lidos
 		}
 			
-		printf("Verificar mais CPF? 1-sim/2-n„o\n");
+		printf("Verificar mais CPF? 1-sim/2-n√£o\n");
 		scanf("%d", &resp);
 		while((resp < 1) || (resp > 2)){
-			printf("Escolha uma das opÁıes!!! 1-SIM/2-N√O\n");
+			printf("Escolha uma das op√ß√µes!!! 1-SIM/2-N√ÉO\n");
 			scanf("%d", &resp);
 		}
 		system("cls");
@@ -76,7 +74,7 @@ main(){
 	porcpfs[2] = (porcpfs[0]/cpfsteste) * 100;
 	porcpfs[3] = (porcpfs[1]/cpfsteste) * 100;
 	printf("CPFs testados foram: [%d]\n", cpfsteste);
-	printf("CPFs v·lidos: [%.0f]\nCPFs inv·lidos: [%.0f]\n", porcpfs[0], porcpfs[1]);
-	printf("A porcentagem de CPFs v·lidos È: [%.1f%%]\n", porcpfs[2]);
-	printf("A porcentagem de CPFs inv·lidos È: [%.1f%%]\n", porcpfs[3]);	
+	printf("CPFs v√°lidos: [%.0f]\nCPFs inv√°lidos: [%.0f]\n", porcpfs[0], porcpfs[1]);
+	printf("A porcentagem de CPFs v√°lidos √©: [%.1f%%]\n", porcpfs[2]);
+	printf("A porcentagem de CPFs inv√°lidos √©: [%.1f%%]\n", porcpfs[3]);	
 }
